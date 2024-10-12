07af8e398dddd60b5b6aac8ff83a3bb217ecc6cb
naufalF21 <naufalfirman098@gmail.com>
2024-10-12 11:38:54 +0700

    feat: add header

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+	return (
+		<div className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+			<div className="mx-auto grid w-full max-w-6xl gap-2">
+				<h1 className="text-3xl font-semibold">Settings</h1>
+			</div>
+		</div>
+	);
+}
