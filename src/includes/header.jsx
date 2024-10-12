1442f0a1bae759f83e4ae627eef7d70847f295a6
naufalF21 <naufalfirman098@gmail.com>
2024-10-12 11:55:08 +0700

    feat: integrated Clerk auth on header

--- src/includes/header.jsx
+++ src/includes/header.jsx
@@ -1,61 +1,31 @@
 import Link from 'next/link';
-import { BringToFront, CircleUser, Menu } from 'lucide-react';
+import { BringToFront, Menu } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sheet, SheetTrigger } from '@/components/ui/sheet';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 
 export function Header() {
 	return (
 		<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-			<nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-				<Link
-					href="#"
-					className="flex items-center gap-2 text-lg font-semibold md:text-base"
-				>
-					<BringToFront className="h-6 w-6" />
-					<span>Quizly</span>
-				</Link>
-			</nav>
 			<Sheet>
 				<SheetTrigger asChild>
-					<Button variant="outline" size="icon" className="shrink-0 md:hidden">
-						<Menu className="h-5 w-5" />
-						<span className="sr-only">Toggle navigation menu</span>
-					</Button>
-				</SheetTrigger>
-				<SheetContent side="left">
 					<nav className="grid gap-6 text-lg font-medium">
 						<Link href="#" className="flex items-center gap-2 text-lg font-semibold">
 							<BringToFront className="h-6 w-6" />
 							<span>Quizly</span>
 						</Link>
 					</nav>
-				</SheetContent>
+				</SheetTrigger>
 			</Sheet>
 			<div className="flex justify-end w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="secondary" size="icon" className="rounded-full">
-							<CircleUser className="h-5 w-5" />
-							<span className="sr-only">Toggle user menu</span>
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuLabel>My Account</DropdownMenuLabel>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>Settings</DropdownMenuItem>
-						<DropdownMenuItem>Support</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>Logout</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
+				<SignedOut>
+					<Button>
+						<SignInButton />
+					</Button>
+				</SignedOut>
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
 			</div>
 		</header>
 	);
